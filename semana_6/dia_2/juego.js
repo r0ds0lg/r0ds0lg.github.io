57d1676b7be02c31706f9c2ec9f88ab957dd5d78
Gabriela Rodríguez <grs@MacBook-Air-de-Gabriela.local>
2018-10-17 01:25:55 -0500

    Juego dia 2, avance y actualizacion de portafolio

---
+++ semana_6/dia_2/juego.js
@@ -0,0 +1,29 @@
+/*
+    Objetivo de hoy lunes:
+
+    1. Planeación de su juego, decidir que
+    tema llevará y obtener imagenes 
+    representativas de las opciones que
+    se usarán
+
+    2. Crear layout de su juego con las
+    siguientes características:
+      - Encabezado con nombre de juego
+      - Colores personalizados de fondo
+        y textos
+      - Botones grandes con las 3 opciones
+        del juego (piedra, papel, tijera)
+      - Cuadro de texto que muestre los 
+        resultados del juego (estilos)
+
+    Recuerden utilizar flexbox para su
+    estructura de elementos.
+
+    Ideas de temas alternos:
+    
+    En ciertos paises asiaticos juegan con:
+        pajaro, agua y piedra
+
+    Versión difícil (ver alt.png):
+    piedra, papel, tijera, lagartija, spock
+*/
