a4cc94974b3d3409976ce230305320c2d4ee8bb3
r0ds0lg <rod.sol.gab@gmail.com>
2018-11-06 16:40:51 -0600

    organizando index js

--- semana_9/dia_1/index.js
+++ semana_9/dia_1/index.js
@@ -1,29 +1,40 @@
+//referencias 
+let divModal = document.querySelector('.modalBackground');
+let closeMbtn = document.querySelector('.closeBtn');
 
+//Funciones
 
+//Detecta source
 let src = function imageSelect (e){
     src = e.target.src;
     if (!src) return 
     console.log(src);
+    openModal()
+}
+
+//Muestra imagen
+function openModal () {
     document.querySelector('.openModal').style.backgroundImage = "url("+ src + ")";
     divModal.classList.add('visible');
 }
 
-
-let divModal = document.querySelector('.modalBackground');
-let closeMbtn = document.querySelector('.closeBtn');
-
+//Cierra modal
 function closeModal() {
 divModal.classList.remove('visible');
 } 
 
+//Detecta tecla Escape y corre funcion para cerrar modal
+function detectarEsc (e) {
+    if (e.key === "Escape"){
+    closeModal();
+    }
+}
+
+//Eventos
 closeMbtn.addEventListener('click', closeModal);
 document.addEventListener('keyup', detectarEsc);
 document.addEventListener ('click', src);
 
 
 
-function detectarEsc (e) {
-    if (e.key === "Escape"){
-    closeModal();
-    }
-}
+
